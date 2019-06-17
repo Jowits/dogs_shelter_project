6892e32e2e6a630d6992aed25417c724d0b93a93
Jowita <jowita.lapies@gmail.com>
2019-06-17 14:37:58 +0100

    change in seeds file with Leash.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 
 
- owner = 100.times do 
+ 100.times do 
     Dog.create(
         name: Faker::Creature::Dog.unique.name,
         age: Faker::Creature::Dog.age,
@@ -10,24 +10,16 @@
     )
 end
  
-dog = 100.times do 
+100.times do 
     Owner.create(
         name: Faker::Name.unique.name
     )
 end 
 
-def generate_owner
-    owner_array= Owner.all.select {|owners| owners.id}
-    owner_array[rand(owner_array.length)]
-end 
 
-def generate_dog
-    owner_array= Dog.all.select{|dogs| dogs.id}
-    owner_array[rand(owner_array.length)]
-end 
  
-10.times do
-    Leash.create(dog_id: generate_dog, owner_id: generate_owner)
+20.times do
+    Leash.create(dog_id: Dog.all.to_a.sample.id, owner_id: Owner.all.to_a.sample.id)
 end
 
 
