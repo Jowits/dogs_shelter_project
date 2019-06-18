0f8243c343e5defdcbb81e9f7d463cc380970fbc
Jowita <jowita.lapies@gmail.com>
2019-06-18 11:55:21 +0100

    changes_11-55

--- bin/run.rb
+++ bin/run.rb
@@ -7,5 +7,5 @@ cli = CommandLineInterface.new
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+cli.welcome
+ #leave this here to ensure binding.pry isn't the last line
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,24 @@
-
+require "rainbow"
+$prompt = TTY::Prompt.new
 class CommandLineInterface
-    $prompt = TTY::Prompt.new
-
+    def welcome
+        
+        puts Rainbow(" ______ ").red
+        puts Rainbow("| ___ \ ").orange
+        puts Rainbow("| |_/ / __ _  __      __  ___   ").yellow
+        puts Rainbow("|  __/ / _` | \ \ /\ / / / __|  ").green
+        puts Rainbow("| |   | (_| |  \ V  V /  \__ \  ").aqua
+        puts Rainbow("\_|    \__,_|   \_/\_/   |___/  ").purple
+        puts Rainbow("===========================================").purple
+        puts Rainbow("  _     /)---(\          /~~~\             ").yellow
+        puts Rainbow("  \\   (/ . . \)        /  .. \            ").yellow
+        puts Rainbow("   \\__)-\(*)/         (_,\  |_)           ").yellow
+        puts Rainbow("   \_       (_         /   \@/    /^^^\    ").yellow
+        puts Rainbow("   (___/-(____) _     /      \   / . . \   ").yellow
+        puts Rainbow("                \\   /  `    |   V\ Y /V   ").yellow
+        puts Rainbow("                 \\/  \   | _\    / - \    ").yellow
+        puts Rainbow("                  \   /__'|| \\_  |    \   ").yellow
+        puts Rainbow("                   \_____)|_).\_).||(__V   ").yellow
+   
+    end
 end
