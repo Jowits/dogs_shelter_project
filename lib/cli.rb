aa351d65fe8c0206c4794c07629d5a7c2a99d282
Jowita <jowita.lapies@gmail.com>
2019-06-19 19:22:28 +0100

    added bulgod to stories

--- lib/cli.rb
+++ lib/cli.rb
@@ -104,24 +104,7 @@ class CommandLineInterface
       puts "#{name} is now up for adoption!"
     end 
 
-    $stories = {
-    "See Ricky the pitbull's story: from sleeping in central park to a warm Brooklyn home " => 1,
-    "See Classy golden retriever's story: from bait dog in a dumpster to forever home " => 2,
-    "See DJ the dachshund's story: from eating out of the sidewalk trash to eating out of the kitchen trash" => 3,
-    'Exit' => 0
-  }
-
-    def dog_stories
-      input = $prompt.select("Select an option:", $stories)
-      case input
-      when 1
-        puts "Adopted this guy from a pound/rescue. He was bought by someone from a breeder and because they didn't realize how much work a dog can be, they left him tied in central park one night. He's a troublemaker, but he's also a sweet boy. When I first met him, he began tugging at my shoelaces and eventually curled up in my lap and went to sleep. I fell in love and adopted him that day."  
-      when 2
-        puts "I'd been working on convincing my mom to rescue a dog for about six months. She kept saying she knew it was irrational, but that she just couldn't trust them. But then she saw Classy on the local shelter's Facebook page. Classy had been used as a bait dog, then left to die behind a dumpster. After a year in medical foster, she was ready to get a forever home. When we first saw a picture of her, the gash on her neck was so bad we thought she'd been hanged. We officially adopted Classy on Christmas Day, giving her a forever home."
-      when 3
-        puts "In the seven months we have had him, he has overcome his fear of stairs, squeaky toys, and the dishwasher. He has learned how to interact with other dogs confidently, how to play, and how to follow commands. He is much more confident and happy than he was when we first got him, and we are so happy he is a part of our lives forever."  
-      end
-    end 
+    
 
 $choices = {
     'Choose dog by size' => 1,
